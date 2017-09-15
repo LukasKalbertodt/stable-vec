b3943128ef92e9046310cd4ce4e149b3fd019ecc
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-09-15 18:46:36 +0200

    Add comments to `correct_drop` example

--- examples/correct_drop.rs
+++ examples/correct_drop.rs
@@ -2,7 +2,7 @@ extern crate stable_vec;
 
 use stable_vec::StableVec;
 
-
+/// A dummy type which prints its character when dropped.
 struct EchoDrop(pub char);
 
 impl Drop for EchoDrop {
@@ -14,11 +14,20 @@ impl Drop for EchoDrop {
 fn main() {
     let mut sv = StableVec::new();
     sv.push(EchoDrop('a'));
-    let b = sv.push(EchoDrop('b'));
+    let b_idx = sv.push(EchoDrop('b'));
     sv.push(EchoDrop('c'));
 
-    println!("--- removing 'b' ...");
-    sv.remove(b);
+    {
+        // Removing it from the vector shouldn't drop the value: it is moved
+        // out the vector into this function.
+        println!("--- removing 'b' (nothing should be dropped!) ...");
+        let b = sv.remove(b_idx);
 
-    println!("--- going out of scope now ...");
+        // But now the value goes out of scope and it should be dropped now.
+        println!("--- letting 'b' go out of scope (it should be dropped now!) ...");
+    }
+
+    // The vector will be dropped at the end of this function and should drop
+    // all elements inside it which haven't been removed yet ('a' and 'c').
+    println!("--- Letting 'sv' go out of scope (it should drop 'a' and 'c'!) ...");
 }

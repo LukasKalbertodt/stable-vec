1fa65a029a5b3e9ea10e2e187d68491a443185df
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-04-10 21:17:37 +0200

    Add `OptionCore` implementation of `Core`

---
+++ src/core/option.rs
@@ -0,0 +1,145 @@
+use std::{
+    cmp,
+    hint::unreachable_unchecked,
+    mem::ManuallyDrop,
+    ops::DerefMut,
+    ptr,
+};
+
+use super::Core;
+
+/// A `Core` implementation that is essentially a `Vec<Option<T>>`.
+///
+/// TODO: explain advantages and disadvantages.
+#[derive(Clone)]
+pub struct OptionCore<T> {
+    data: Box<[ManuallyDrop<Option<T>>]>,
+    used_len: usize,
+}
+
+impl<T> Core<T> for OptionCore<T> {
+    fn new() -> Self {
+        Self {
+            data: Box::default(),
+            used_len: 0,
+        }
+    }
+
+    fn from_vec(vec: Vec<T>) -> Self {
+        let mut out = Self::new();
+        out.grow(vec.len());
+
+        out.used_len = vec.len();
+        for (i, elem) in vec.into_iter().enumerate() {
+            // Due to the `grow` above we know that `i` is always greater than
+            // `out.capacity()`. And because we started with an empty
+            // instance, all elements start out as deleted.
+            unsafe {
+                out.insert_at(i, elem);
+            }
+        }
+
+        out
+    }
+
+    fn used_len(&self) -> usize {
+        self.used_len
+    }
+
+    unsafe fn set_used_len(&mut self, v: usize) {
+        self.used_len = v;
+    }
+
+    fn capacity(&self) -> usize {
+        self.data.len()
+    }
+
+    fn grow(&mut self, additional: usize) {
+        let new_cap = self.used_len + additional;
+        if self.capacity() >= new_cap {
+            return;
+        }
+
+        // We at least double our capacity. Otherwise repeated `push`es are
+        // O(n²).
+        //
+        // This multiplication can't overflow, because we know the capacity is
+        // below `isize::MAX` (`Vec` ensures this).
+        let new_cap = cmp::max(new_cap, 2 * self.capacity());
+
+        let mut new: Vec<ManuallyDrop<Option<T>>> = Vec::with_capacity(new_cap);
+
+        // Copy all old elements over to the new vector. After we do this, we
+        // can just drop the box which will deallocate the old memory block,
+        // but not touch the old values anymore (thanks to `ManuallyDrop`).
+        unsafe {
+            ptr::copy_nonoverlapping(self.data.as_ptr(), new.as_mut_ptr(), self.data.len());
+            new.set_len(self.data.len());
+        }
+
+        // Fill the rest of the vector with deleted elements.
+        new.resize_with(new_cap, || ManuallyDrop::new(None));
+
+        self.data = new.into_boxed_slice();
+    }
+
+    unsafe fn has_element_at(&self, idx: usize) -> bool {
+        self.data.get_unchecked(idx).is_some()
+    }
+
+    unsafe fn insert_at(&mut self, idx: usize, elem: T) {
+        *self.data.get_unchecked_mut(idx) = ManuallyDrop::new(Some(elem));
+    }
+
+    unsafe fn remove_at(&mut self, idx: usize) -> T {
+        match self.data.get_unchecked_mut(idx).deref_mut().take() {
+            None => unreachable_unchecked(),
+            Some(elem) => elem,
+        }
+    }
+
+    unsafe fn get_unchecked(&self, idx: usize) -> &T {
+        match &**self.data.get_unchecked(idx) {
+            None => unreachable_unchecked(),
+            Some(elem) => elem,
+        }
+    }
+
+    unsafe fn get_unchecked_mut(&mut self, idx: usize) -> &mut T {
+        match &mut **self.data.get_unchecked_mut(idx) {
+            None => unreachable_unchecked(),
+            Some(elem) => elem,
+        }
+    }
+
+    fn clear(&mut self) {
+        // We can assume that all existing elements have an index lower than
+        // `used_len` (this is one of the invariants of the `Core` interface).
+        for idx in 0..self.used_len {
+            unsafe {
+                // Call `Option::take` to overwrite everything with `None` and
+                // drop all existing values.
+                self.data.get_unchecked_mut(idx).deref_mut().take();
+            }
+        }
+        self.used_len = 0;
+    }
+
+    fn next_index_from(&self, idx: usize) -> Option<usize> {
+        (idx..self.used_len)
+            .find(|&idx| self.data[idx].is_some())
+    }
+
+    fn prev_index_from(&self, idx: usize) -> Option<usize> {
+        (0..=idx)
+            .rev()
+            .find(|&idx| self.data[idx].is_some())
+    }
+}
+
+impl<T> Drop for OptionCore<T> {
+    fn drop(&mut self) {
+        // Drop all elements
+        self.clear();
+    }
+}

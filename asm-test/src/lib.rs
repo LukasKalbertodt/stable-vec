01a49ffca18278a687468d2b810fe130c313ee15
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-04-13 14:26:52 +0200

    Add more assembly test functions

--- asm-test/src/lib.rs
+++ asm-test/src/lib.rs
@@ -1,16 +1,31 @@
 //! This is just a dummy library to check the assembly output of some
 //! functions.
 
-use stable_vec::StableVec;
+use stable_vec::{StableVec, OptionCore, BitVecCore};
 
-pub fn index_u32_option(sv: &StableVec<u32>, index: usize) -> u32 {
+type SvOption<T> = StableVec<T, OptionCore<T>>;
+type SvBitVec<T> = StableVec<T, BitVecCore<T>>;
+
+pub fn index_u32_option(sv: &SvOption<u32>, index: usize) -> u32 {
     sv[index]
 }
 
-pub fn push_u32_option(sv: &mut StableVec<u32>, val: u32) -> usize {
+pub fn push_u32_option(sv: &mut SvOption<u32>, val: u32) -> usize {
     sv.push(val)
 }
 
-pub fn count_u32_option(sv: &StableVec<u32>) -> usize {
+pub fn count_u32_option(sv: &SvOption<u32>) -> usize {
+    sv.indices().count()
+}
+
+pub fn index_u32_bitvec(sv: &SvBitVec<u32>, index: usize) -> u32 {
+    sv[index]
+}
+
+pub fn push_u32_bitvec(sv: &mut SvBitVec<u32>, val: u32) -> usize {
+    sv.push(val)
+}
+
+pub fn count_u32_bitvec(sv: &SvBitVec<u32>) -> usize {
     sv.indices().count()
 }

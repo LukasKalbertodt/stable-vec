79641c32a8fbe352bdfa632c5b069c907f723f62
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-07-27 12:40:33 +0200

    Adjust CI to run Miri tests on nightly
    A few other things changed too:
    - check style is its own step now and is only executed once
    - nightly is not allowed to fail anymore
    - main script is now inline in `.travis.yml`
    - Do not use cache anymore

---
+++ ci/run-miri-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Exit script on the first error
+set -o errexit -o nounset
+
+MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/miri)
+echo "Installing latest nightly with Miri: $MIRI_NIGHTLY"
+rustup default "$MIRI_NIGHTLY"
+
+rustup component add miri
+cargo miri setup
+
+cargo clean
+cargo miri test
